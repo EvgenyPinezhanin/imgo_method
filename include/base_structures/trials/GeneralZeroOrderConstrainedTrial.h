d2e3c8f509a76cd8284f4471a596476c7fc197cd
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-07-08 22:23:10 +0300

    add base trials

---
+++ include/base_structures/trials/GeneralZeroOrderConstrainedTrial.h
@@ -0,0 +1,17 @@
+#ifndef GENERAL_ZERO_ORDER_CONSTRAINED_TRIAL_H_
+#define GENERAL_ZERO_ORDER_CONSTRAINED_TRIAL_H_
+
+#include <vector>
+
+#include <base_structures/trials/GeneralZeroOrderTrial.h>
+
+using std::vector;
+
+struct GeneralZeroOrderConstrainedTrial : public GeneralZeroOrderTrial {
+    vector<double> g;
+
+    GeneralZeroOrderConstrainedTrial(double _x = 0.0, double _z = 0.0, const vector<double> &_g)
+                                    : GeneralZeroOrderTrial(_x, _z), g(_g) {};
+};
+
+#endif // GENERAL_ZERO_ORDER_CONSTRAINED_TRIAL_H_
---
+++ include/base_structures/trials/GeneralZeroOrderTrial.h
@@ -0,0 +1,10 @@
+#ifndef GENERAL_ZERO_ORDER_TRIAL_H_
+#define GENERAL_ZERO_ORDER_TRIAL_H_
+
+struct GeneralZeroOrderTrial {
+    double x, z;
+
+    GeneralZeroOrderTrial(double _x = 0.0, double _z = 0.0) : x(_x), z(_z) {};
+};
+
+#endif // GENERAL_ZERO_ORDER_TRIAL_H_
