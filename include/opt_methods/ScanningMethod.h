0dd79a1c09ed1564e0ea54be96ba45bff0194458
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-07-10 00:10:29 +0300

    add ScanningMethod

---
+++ include/opt_methods/ScanningMethod.h
@@ -0,0 +1,39 @@
+#ifndef SCANNING_METHOD_H_
+#define SCANNING_METHOD_H_
+
+#include <base_classes/opt_methods/ICharacteristicOptMethod.h>
+#include <base_classes/opt_methods/IGeneralNumericalOptMethod.h>
+#include <base_structures/result_methods/GeneralNumericalResultMethod.h>
+#include <opt_tasks/OneDimensionalTask.h>
+#include <trials/Trial.h>
+
+using StoppingCondition = GeneralNumericalStoppingCondition;
+using ResultMethod = GeneralNumericalResultMethod<StoppingCondition, double>;
+
+class ScanningMethod : public ICharacteristicOptMethod<Trial>,
+    public IGeneralNumericalOptMethod<double, Trial, OneDimensionalTask, ResultMethod, double>  {
+private:
+    void insertInSorted(const Trial &trial) override;
+
+    void calcCharacteristic() override;
+
+    Trial newTrial(const double &x) override;
+    double selectNewPoint() override;
+
+    double estimateSolution() const override;
+
+    bool stopConditions() override;
+    bool stopConditionsTest() override;
+
+public:
+    ScanningMethod(const OneDimensionalTask &_task = OneDimensionalTask(), double _accuracy = 0.001,
+                   double _error = 0.001, int _maxTrials = 1000, int _maxFevals = 1000)
+                  : ICharacteristicOptMethod<Trial>(), IGeneralNumericalOptMethod<double, Trial,
+                  OneDimensionalTask, ResultMethod, double>(_task, _accuracy, _error, _maxTrials, _maxFevals) {};
+
+    void solve(ResultMethod &result) override;
+
+    bool solveTest(ResultMethod &result) override;
+};
+
+#endif // SCANNING_METHOD_H_
---
+++ src/ScanningMethod.cpp
@@ -0,0 +1,144 @@
+#include <opt_methods/ScanningMethod.h>
+
+#include <algorithm>
+#include <limits>
+#include <iterator>
+
+#include <myMath.h>
+
+using std::numeric_limits;
+using std::advance;
+using std::distance;
+using std::min_element;
+using std::max;
+using std::abs;
+
+const double epsilon = 1e-14;
+
+void ScanningMethod::insertInSorted(const Trial &trial) {
+    vector<Trial>::iterator iter = trialPoints.begin();
+
+    advance(iter, t);
+    trialPoints.insert(iter, trial);
+}
+
+void ScanningMethod::calcCharacteristic() {
+    double R = -numeric_limits<double>::infinity(), Rtmp;
+    
+    int trialPointsSize = (int)trialPoints.size();
+    for (int i = 1; i < trialPointsSize; i++) {
+        Rtmp = trialPoints[i].x - trialPoints[i - 1].x;
+        if (Rtmp > R) {
+            R = Rtmp;
+            t = i;
+        }
+    }
+}
+
+Trial ScanningMethod::newTrial(const double &x) {
+    numberFevals++;
+    return Trial(x, task.computeObjFunction(x));
+}
+
+double ScanningMethod::selectNewPoint() {
+    // Steps 2, 3
+    calcCharacteristic();
+
+    // Step 4
+    return 0.5 * (trialPoints[t].x + trialPoints[(size_t)t - 1].x);
+}
+
+double ScanningMethod::estimateSolution() const {
+    auto iter = min_element(trialPoints.begin(), trialPoints.end(),
+    [] (const Trial &trial1, const Trial &trial2) {
+        return trial1.z < trial2.z;
+    });
+
+    return iter->x;
+}
+
+bool ScanningMethod::stopConditions() {
+    if (trialPoints[t].x - trialPoints[(size_t)t - 1].x <= accuracy) {
+        result.stoppingCondition = StoppingCondition::accuracy;
+        return true;
+    }
+    if (numberFevals >= maxFevals) {
+        result.stoppingCondition = StoppingCondition::maxFevals;
+        return true;
+    }
+    if (numberTrials >= maxTrials) {
+        result.stoppingCondition = StoppingCondition::maxTrials;
+        return true;
+    }
+    return false;
+}
+
+bool ScanningMethod::stopConditionsTest() {
+    if (abs(trialPoints[t].x - task.getOptimalPoint()) <= error) {
+        result.stoppingCondition = StoppingCondition::error;
+        return true;
+    }
+    return stopConditions();
+}
+
+void ScanningMethod::solve(ResultMethod &_result) {
+    trialPoints.clear();
+    numberFevals = 0;
+
+    result = _result;
+
+    trialPoints.push_back(newTrial(task.getSearchArea().getLowerBound()));
+    Trial trial = newTrial(task.getSearchArea().getUpBound());
+
+    numberTrials = 2;
+
+    double xNew;
+    while(true) {
+        // Step 1
+        insertInSorted(trial);
+
+        // Steps 2, 3, 4
+        xNew = selectNewPoint();
+
+        trial = newTrial(xNew);
+        numberTrials++;
+
+        if (stopConditions()) break;
+    }
+    
+    result.numberTrials = numberTrials;
+    result.numberFevals = numberFevals;
+    result.solution = estimateSolution();
+}
+
+bool ScanningMethod::solveTest(ResultMethod &_result) {
+    trialPoints.clear();
+    numberFevals = 0;
+
+    result = _result;
+
+    trialPoints.push_back(newTrial(task.getSearchArea().getLowerBound()));
+    Trial trial = newTrial(task.getSearchArea().getUpBound());
+
+    numberTrials = 2;
+
+    double xNew;
+    while (true) {
+        // Step 1
+        insertInSorted(trial);
+
+        // Steps 2, 3, 4
+        xNew = selectNewPoint();
+
+        trial = newTrial(xNew);
+        numberTrials++;
+
+        if (stopConditionsTest()) break;
+    }
+    
+    result.numberTrials = numberTrials;
+    result.numberFevals = numberFevals;
+    result.solution = estimateSolution();
+
+    return result.stoppingCondition == StoppingCondition::error;
+}
