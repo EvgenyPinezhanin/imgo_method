76a64b7130a2742c9167f5ca36d5d392bc615c92
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-07-10 14:27:04 +0300

    add lipschitzConstant

--- include/opt_tasks/OneDimensionalTask.h
+++ include/opt_tasks/OneDimensionalTask.h
@@ -9,12 +9,18 @@
 using std::function;
 
 class OneDimensionalTask : public IGeneralOptTask<function<double(double)>, OneDimensionalSearchArea, double> {
+private:
+    double lipschitzConstant;
+
 public:
     OneDimensionalTask(const function<double(double)> &_objFunction = nullptr,
                        const OneDimensionalSearchArea &_area = OneDimensionalSearchArea(0.0, 1.0),
-                       double _optPoint = 0.0)
+                       double _optPoint = 0.0, double _lipschitzConstant = -1.0)
                       : IGeneralOptTask<function<double(double)>, OneDimensionalSearchArea, double>(_objFunction,
-                      _area, _optPoint) {};
+                      _area, _optPoint), lipschitzConstant(_lipschitzConstant) {};
+
+    void setLipschitzConstant(double _lipschitzConstant) { lipschitzConstant = _lipschitzConstant; };
+    double getLipschitzConstant() const { return lipschitzConstant; };
 
     double computeObjFunction(double x) const override { return objFunction(x); };
 };
