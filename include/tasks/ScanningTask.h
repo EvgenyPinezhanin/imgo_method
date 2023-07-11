d455c602bb13e971572e52b872aa4683c61fc1e2
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-07-11 21:12:57 +0300

    add alias ScanningTask

---
+++ include/tasks/ScanningTask.h
@@ -0,0 +1,9 @@
+#ifndef SCANNING_TASK_H_
+#define SCANNING_TASK_H_
+
+#include <base_structures/tasks/GeneralNumericalTask.h>
+#include <opt_tasks/OneDimensionalTask.h>
+
+using ScanningTask = GeneralNumericalTask<OneDimensionalTask>;
+
+#endif // SCANNING_TASK_H_
