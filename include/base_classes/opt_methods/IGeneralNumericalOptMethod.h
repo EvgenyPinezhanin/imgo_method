f9d8a5b433ffe19b3cbc8b887dda1d5e9709aacb
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-07-22 15:09:34 +0300

    delete SolutionType, change estimateSolution, delete maxFevals in IGeneralOptMethod

--- include/base_classes/opt_methods/IGeneralNumericalOptMethod.h
+++ include/base_classes/opt_methods/IGeneralNumericalOptMethod.h
@@ -8,7 +8,7 @@
 using std::vector;
 
 namespace om {
-    template <typename SolutionType, typename TrialType, typename OptProblemType, typename ResultMethodType, typename PointType>
+    template <typename TrialType, typename PointType, typename OptProblemType, typename ResultMethodType>
     class IGeneralNumericalOptMethod : public IGeneralOptMethod<OptProblemType, ResultMethodType> {
     protected:
         vector<TrialType> trialPoints;
@@ -20,16 +20,15 @@ namespace om {
         virtual TrialType newTrial(const PointType &x) = 0;
         virtual PointType selectNewPoint() = 0;
 
-        virtual SolutionType estimateSolution() const = 0;
+        virtual double estimateSolution(PointType &x) const = 0;
 
         virtual bool stopConditions() = 0;
         virtual bool stopConditionsTest() = 0;
 
     public:
         IGeneralNumericalOptMethod(const OptProblemType &_problem, double _accuracy, double _error, int _maxTrials, int _maxFevals)
-                                  : IGeneralOptMethod<OptProblemType, ResultMethodType>(_problem, _maxFevals), trialPoints(0),
-                                  accuracy(_accuracy), error(_error), maxTrials(_maxTrials), numberTrials(0), maxFevals(_maxFevals),
-                                  numberFevals(0) {};
+                                  : IGeneralOptMethod<OptProblemType, ResultMethodType>(_problem), trialPoints(0), accuracy(_accuracy),
+                                  error(_error), maxTrials(_maxTrials), numberTrials(0), maxFevals(_maxFevals), numberFevals(0) {};
 
         void setAccuracy(double _accuracy) { accuracy = _accuracy; };
         double getAccuracy() const { return accuracy; };
