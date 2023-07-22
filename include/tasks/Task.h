c80f77c7391ae1d251b5d37fdac12381b4e7ea9f
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-07-22 15:38:03 +0300

    add namespace om

--- include/tasks/Task.h
+++ include/tasks/Task.h
@@ -3,14 +3,14 @@
 
 #include <base_structures/tasks/GeneralNumericalTask.h>
 
-template <typename OptimizationProblemType>
-struct Task : public GeneralNumericalTask<OptimizationProblemType> {
+template <typename OptProblemType>
+struct Task : public om::GeneralNumericalTask<OptProblemType> {
     int blockNumber, functionNumber;
 
-    Task(string _name, const OptimizationProblemType &_optProblem, int _blockNumber,
-         int _functionNumber, double _accuracy, double _error, int _maxTrials, int _maxFevals, bool _use = true)
-        : GeneralNumericalTask<OptimizationProblemType>(_name, _optProblem, _accuracy, _error, _maxTrials,
-        _maxFevals, _use), blockNumber(_blockNumber), functionNumber(_functionNumber) {};
+    Task(string _name, const OptProblemType &_optProblem, int _blockNumber, int _functionNumber,
+         double _accuracy, double _error, int _maxTrials, int _maxFevals, bool _use = true)
+        : om::GeneralNumericalTask<OptProblemType>(_name, _optProblem, _accuracy, _error,
+        _maxTrials, _maxFevals, _use), blockNumber(_blockNumber), functionNumber(_functionNumber) {};
 };
 
 #endif // TASK_H_
