07b021deb2f16d9ca7d0a1a4a2f2255faa040d2e
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-09-03 23:15:25 +0300

    Implemented interface for index scheme

---
+++ include/base_classes/opt_methods/IIndexSchemeOptMethod.h
@@ -0,0 +1,37 @@
+#ifndef I_INDEX_SCHEME_OPT_METHOD_H_
+#define I_INDEX_SCHEME_OPT_METHOD_H_
+
+#include <vector>
+
+#include <base_structures/trials/IndexTrial.h>
+
+using std::vector;
+
+namespace opt {
+    class IIndexSchemeOptMethod {
+    protected:
+        vector<double> reliability;
+        vector<double> constantsEstimation;
+
+        vector<vector<IndexTrial>> I;
+        vector<double> zValues;
+
+        virtual void estimatingConstants() = 0;
+
+        virtual void calcI() = 0;
+        virtual void calcZValues() = 0;
+
+    public:
+        IIndexSchemeOptMethod(const vector<double> &_reliability)
+                              : reliability(_reliability), constantsEstimation(0), I(0), zValues(0) {};
+
+        void setReliability(const vector<double> &_reliability) { reliability = _reliability; };
+        void getReliability(vector<double>& _reliability) const { _reliability = reliability; };
+
+        void getConstantEstimation(const vector<double> &_constantsEstimation) const {
+            _constantsEstimation = constantsEstimation;
+        };
+    };
+}
+
+#endif // I_INDEX_SCHEME_OPT_METHOD_H_
