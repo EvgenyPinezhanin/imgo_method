34761476045151e4f91dc47dd6abe7d3305f0dfa
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-09-03 23:13:05 +0300

    Added typename ParametersMethod for template

--- include/base_classes/opt_methods/IGeneralNumericalOptMethod.h
+++ include/base_classes/opt_methods/IGeneralNumericalOptMethod.h
@@ -8,14 +8,14 @@
 using std::vector;
 
 namespace opt {
-    template <typename TrialType, typename PointType, typename OptProblemType, typename ResultMethodType>
-    class IGeneralNumericalOptMethod : public IGeneralOptMethod<OptProblemType, ResultMethodType> {
+    template <typename TrialType, typename PointType, typename OptProblemType,
+              typename ParametersMethod, typename ResultMethodType>
+    class IGeneralNumericalOptMethod:
+        public IGeneralOptMethod<OptProblemType, ParametersMethod, ResultMethodType> {
     protected:
         vector<TrialType> trialPoints;
 
-        double accuracy, error;
-        int maxTrials, numberTrials;
-        int maxFevals, numberFevals;
+        int numberTrials, numberFevals;
 
         virtual TrialType newTrial(const PointType &x) = 0;
         virtual PointType selectNewPoint() = 0;
@@ -26,24 +26,29 @@ namespace opt {
         virtual bool stopConditionsTest() = 0;
 
     public:
-        IGeneralNumericalOptMethod(const OptProblemType &_problem, double _accuracy, double _error, int _maxTrials, int _maxFevals)
-                                  : IGeneralOptMethod<OptProblemType, ResultMethodType>(_problem), trialPoints(0), accuracy(_accuracy),
-                                  error(_error), maxTrials(_maxTrials), numberTrials(0), maxFevals(_maxFevals), numberFevals(0) {};
+        IGeneralNumericalOptMethod(const OptProblemType &_problem, const ParametersMethod &_parameters):
+            IGeneralOptMethod<OptProblemType, ParametersMethod, ResultMethodType>(_problem, _parameters),
+            trialPoints(0),
+            numberTrials(0),
+            numberFevals(0)
+        {};
 
-        void setAccuracy(double _accuracy) { accuracy = _accuracy; };
-        double getAccuracy() const { return accuracy; };
+        void setAccuracy(double _accuracy) { parameters.accuracy = _accuracy; };
+        double getAccuracy() const { return parameters.accuracy; };
 
-        void setError(double _error) { error = _error; };
-        double getError() const { return error; };
+        void setError(double _error) { parameters.error = _error; };
+        double getError() const { return parameters.error; };
 
-        void setMaxTrials(int _maxTrials) { maxTrials = _maxTrials; };
-        int getMaxTrials() const { return maxTrials; };
+        void setMaxTrials(int _maxTrials) { parameters.maxTrials = _maxTrials; };
+        int getMaxTrials() const { return parameters.maxTrials; };
 
-        void setMaxFevals(int _maxFevals) { maxFevals = _maxFevals; };
-        int getMaxFevals() const { return maxFevals; };
+        void setMaxFevals(int _maxFevals) { parameters.maxFevals = _maxFevals; };
+        int getMaxFevals() const { return parameters.maxFevals; };
 
         void getTrialPoints(vector<TrialType> &_trialPoints) const { _trialPoints = trialPoints; };
         int getNumberTrialPoints() const { return trialPoints.size(); };
+
+        using IGeneralOptMethod<OptProblemType, ParametersMethod, ResultMethodType>::parameters;
     };
 }
 
--- include/base_classes/opt_methods/IGeneralOptMethod.h
+++ include/base_classes/opt_methods/IGeneralOptMethod.h
@@ -2,18 +2,25 @@
 #define I_GENERAL_OPT_METHOD_H_
 
 namespace opt {
-    template <typename OptProblemType, typename ResultMethodType>
+    template <typename OptProblemType, typename ParametersMethod, typename ResultMethodType>
     class IGeneralOptMethod {
     protected:
         OptProblemType problem;
-
+        ParametersMethod parameters;
         ResultMethodType result;
 
     public:
-        IGeneralOptMethod(const OptProblemType &_problem) : problem(_problem), result(ResultMethodType()) {};
+        IGeneralOptMethod(const OptProblemType &_problem, const ParametersMethod &_parameters):
+            problem(_problem),
+            parameters(_parameters),
+            result(ResultMethodType())
+        {};
 
         void setProblem(const OptProblemType &_problem) { problem = _problem; };
         OptProblemType getProblem() const { return problem; };
+
+        void setParameters(const ParametersMethod &_parameters) { parameters = _parameters; };
+        ParametersMethod getParameters() const { return parameters; };
 
         virtual void solve(ResultMethodType &_result) = 0;
         virtual bool solveTest(ResultMethodType &_result) = 0;
