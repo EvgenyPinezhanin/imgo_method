18fbfe9c44ace1c5c62a02c6f6c57f67073efb0a
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-11-21 20:51:20 +0300

    Replaced Trials- & VariablesFile with output_file

--- include/Solver.h
+++ include/Solver.h
@@ -6,7 +6,7 @@
 #include <string>
 
 #include <test_opt_problems/onedimensional_opt_problems.h>
-#include <gnuplot/TrialsFile.h>
+#include <gnuplot/output_file.h>
 #include <omp.h>
 
 template <typename OptMethodType>
@@ -17,53 +17,100 @@ protected:
     typename OptMethodType::Result result;
     typename OptMethodType::Report report;
 
+    output_file trials_file;
+
+    vector<typename OptMethodType::Trial> trials;
+    vector<typename OptMethodType::OptProblem::Point> optimalPoints;
+    typename OptMethodType::OptProblem::Point point;
+
 public:
     Solver(const OptMethodType &_optMethod):
         optMethod(_optMethod),
         result(),
-        report()
+        report(),
+        trials_file()
     {};
 
-    void solve(const vector<typename OptMethodType::Task> &tasks, const string &saveDirectory) {
-        TrialsFile trialsFile;
+    void solveTask(const typename OptMethodType::Task &task, const string &saveDirectory) {
+        if (task.use) {
+            trials_file.open(saveDirectory + blockNames[task.blockNumber] + 
+                             "_" + std::to_string(task.functionNumber));
+            if (!trials_file.is_open()) std::cerr << "trials_file opening error\n";
 
-        vector<typename OptMethodType::Trial> trials;
-        vector<typename OptMethodType::OptProblem::Point> optimalPoints;
-        typename OptMethodType::OptProblem::Point point;
+            optMethod.setProblem(task.problem);
+            optMethod.setParameters(task.parameters);
+
+            double startTime = omp_get_wtime();
+            optMethod.solve(result);
+            double endTime = omp_get_wtime();
+            double workTime = endTime - startTime;
+
+            report.print(std::cout, task, result, workTime);
+
+            task.problem.getOptimalPoints(optimalPoints);
+            trials_file.add_points(optimalPoints, task.problem.getOptimalValue());
+
+            trials_file.add_point(result.point, result.value);
+
+            optMethod.getTrialPoints(trials);
+            trials_file.add_points(trials);
+
+            trials_file.close();
+        }
+    }
+
+    void solveTasks(const vector<typename OptMethodType::Task> &tasks, const string &saveDirectory) {
         int numberTasks = (int)tasks.size();
-        double startTime, endTime, workTime;
 
         double totalStartTime = omp_get_wtime();
         for (int i = 0; i < numberTasks; i++) {
-            if (tasks[i].use) {
-                optMethod.setProblem(tasks[i].problem);
-                optMethod.setParameters(tasks[i].parameters);
-
-                startTime = omp_get_wtime();
-                optMethod.solve(result);
-                endTime = omp_get_wtime();
-                workTime = endTime - startTime;
-
-                report.print(std::cout, tasks[i], result, workTime);
-                std::cout << "\n\n";
-
-                trialsFile.newFile(saveDirectory + blockNames[tasks[i].blockNumber] + 
-                                   "_" + std::to_string(tasks[i].functionNumber));
-                if (!trialsFile.isOpen()) std::cerr << "TrialsFile opening error\n";
-
-                tasks[i].problem.getOptimalPoints(optimalPoints);
-                trialsFile.addPoints(optimalPoints, tasks[i].problem.getOptimalValue());
-
-                trialsFile.addPoint(result.point, result.value);
-
-                optMethod.getTrialPoints(trials);
-                trialsFile.addPoints(trials);
-            }
+            solveTask(tasks[i], saveDirectory);
+            std::cout << "\n\n";
         }
         double totalEndTime = omp_get_wtime();
-        std::cout << "Total time: " << totalEndTime - totalStartTime << "\n";
 
-        trialsFile.closeFile();
+        std::cout << "Total time: " << totalEndTime - totalStartTime << "\n";
+    }
+
+    void solveTestTask(const typename OptMethodType::Task &task, const string &saveDirectory) {
+        if (task.use) {
+            trials_file.open(saveDirectory + blockNames[task.blockNumber] + 
+                             "_" + std::to_string(task.functionNumber));
+            if (!trials_file.is_open()) std::cerr << "trials_file opening error\n";
+
+            optMethod.setProblem(task.problem);
+            optMethod.setParameters(task.parameters);
+
+            double startTime = omp_get_wtime();
+            optMethod.solveTest(result);
+            double endTime = omp_get_wtime();
+            double workTime = endTime - startTime;
+
+            report.print(std::cout, task, result, workTime);
+
+            task.problem.getOptimalPoints(optimalPoints);
+            trials_file.add_points(optimalPoints, task.problem.getOptimalValue());
+
+            trials_file.add_point(result.point, result.value);
+
+            optMethod.getTrialPoints(trials);
+            trials_file.add_points(trials);
+
+            trials_file.close();
+        }
+    }
+
+    void solveTestTasks(const vector<typename OptMethodType::Task> &tasks, const string &saveDirectory) {
+        int numberTasks = (int)tasks.size();
+
+        double totalStartTime = omp_get_wtime();
+        for (int i = 0; i < numberTasks; i++) {
+            solveTestTask(tasks[i], saveDirectory);
+            std::cout << "\n\n";
+        }
+        double totalEndTime = omp_get_wtime();
+
+        std::cout << "Total time: " << totalEndTime - totalStartTime << "\n";
     }
 };
 
