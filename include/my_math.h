9a92de6e8c66bff4d1a7eb162d244724c783731c
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-11-21 20:47:25 +0300

    implemented point and mnk

--- include/my_math.h
+++ include/my_math.h
@@ -10,9 +10,39 @@
 
 #include <vector>
 
-using std::vector;
+struct point {
+    size_t dimension;
 
-double euclideanDistance(const vector<double> &val1, const vector<double> &val2);
+    double x;
+    std::vector<double> y;
+
+    point(double _x)
+        : dimension(1), x(_x), y() {};
+
+    point(double _x, double _y)
+        : dimension(2), x(_x), y(1, _y) {};
+
+    point(double _x, std::vector<double> _y)
+        : dimension(_y.size() + 1), x(_x), y(_y) {};
+};
+
+class mnk {
+private:
+    std::vector<std::vector<double>> A;
+    std::vector<double> b;
+
+public:
+    mnk() : A(), b() {};
+    mnk(const std::vector<std::vector<double>>& _A, const std::vector<double>& _b)
+        : A(_A), b(_b) {};
+
+    void set_A(const std::vector<std::vector<double>>& _A) { A = _A; };
+    void set_b(const std::vector<double>& _b) { b = _b; };
+
+    void solve(std::vector<double> &x);
+};
+
+double euclideanDistance(const std::vector<double> &val1, const std::vector<double> &val2);
 
 template<typename T>
 int sgn(T val) {
--- src/my_math.cpp
+++ src/my_math.cpp
@@ -1,6 +1,43 @@
 #include <my_math.h>
 
-double euclideanDistance(const vector<double> &val1, const vector<double> &val2) {
+void mnk::solve(std::vector<double> &x) {
+    std::vector<std::vector<double>> A_tmp(A);
+    std::vector<double> b_tmp(b);
+    size_t size =  A_tmp.size(), length;
+    double coeff;
+
+    for (size_t i = 0; i < size; ++i) { 
+        for (size_t j = i; j < size; ++j) {
+            if (A_tmp[j][i] != 0) {
+                length = A_tmp[j].size();
+                coeff = A_tmp[j][i];
+                for (size_t k = i; k < length; ++k) {
+                    A_tmp[j][k] /= coeff;
+                }
+                b_tmp[j] /= coeff;
+                for (size_t k = j + 1; k < size; ++k) {
+                    coeff = A_tmp[k][i];
+                    for (size_t l = i; l < size; ++l) {
+                        A_tmp[k][l] -= coeff * A_tmp[j][l];
+                    }
+                    b_tmp[k] -= coeff * b_tmp[j];
+                }
+                swap(A_tmp[j], A_tmp[i]);
+                break;
+            }
+        }
+    }
+
+    for (size_t i = size - 1; i > 0; --i) {
+        for (size_t j = 0; j < i; ++j) {
+            b_tmp[j] -= A_tmp[j][i] * b_tmp[i];
+            A_tmp[j][i] = 0.0;
+        }
+    }
+    x = b_tmp;
+}
+
+double euclideanDistance(const std::vector<double> &val1, const std::vector<double> &val2) {
     double res = 0.0;
     size_t size = val1.size();
     for (int i = 0; i < size; i++) {
