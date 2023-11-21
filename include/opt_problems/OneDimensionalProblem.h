b8c1d6297f3e5d7fa6efcbec47951c942721f65f
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-11-21 20:41:41 +0300

    added setters

--- include/opt_problems/OneDimensionalProblem.h
+++ include/opt_problems/OneDimensionalProblem.h
@@ -1,29 +1,37 @@
 #ifndef ONE_DIMENSIONAL_PROBLEM_H_
 #define ONE_DIMENSIONAL_PROBLEM_H_
 
+#include <vector>
 #include <functional>
 
 #include <base_classes/opt_problems/IGeneralOptProblem.h>
 #include <base_structures/search_areas/OneDimensionalSearchArea.h>
 
-using std::function;
-
-class OneDimensionalProblem : public opt::IGeneralOptProblem<function<double(double)>, opt::OneDimensionalSearchArea, double> {
+class OneDimensionalProblem:
+    public opt::IGeneralOptProblem<std::function<double(double)>, opt::OneDimensionalSearchArea, double>
+{
 private:
     double lipschitzConstant;
 
 public:
-    OneDimensionalProblem(const function<double(double)> &_objFunction = nullptr,
-                          const opt::OneDimensionalSearchArea &_area = opt::OneDimensionalSearchArea(0.0, 1.0),
-                          vector<double> _optimalPoints = vector<double>{}, double _optimalValue = 0.0,
-                          double _lipschitzConstant = -1.0)
-                         : opt::IGeneralOptProblem<function<double(double)>, opt::OneDimensionalSearchArea, double>(_objFunction,
-                         _area, _optimalPoints, _optimalValue), lipschitzConstant(_lipschitzConstant) {};
+    OneDimensionalProblem(const std::function<double(double)> &_object = nullptr,
+                          const opt::OneDimensionalSearchArea &_area = opt::OneDimensionalSearchArea(),
+                          const std::vector<double> &_optimalPoints = std::vector<double>{},
+                          double _optimalValue = 0.0,
+                          double _lipschitzConstant = -1.0):
+        opt::IGeneralOptProblem<std::function<double(double)>, opt::OneDimensionalSearchArea, double>(
+            _object, _area, _optimalPoints, _optimalValue),
+        lipschitzConstant(_lipschitzConstant)
+    {};
+
+    void setSearchArea(const opt::OneDimensionalSearchArea &_area) { area = _area; };
+    void setOptimalPoints(const std::vector<double> &_optimalPoints) { optimalPoints = _optimalPoints; };
+    void setOptimalValue(double _optimalValue) { optimalValue = _optimalValue; };
 
     void setLipschitzConstant(double _lipschitzConstant) { lipschitzConstant = _lipschitzConstant; };
     double getLipschitzConstant() const { return lipschitzConstant; };
 
-    double computeObjFunction(const double &x) const override { return objFunction(x); };
+    double computeObjFunction(const double &x) const override { return object(x); };
 };
 
-#endif // ONE_DIMENSIONAL_OPTIMIZATION_PROBLEM_H_
+#endif // ONE_DIMENSIONAL_PROBLEM_H_
